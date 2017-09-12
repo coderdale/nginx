e240825cfb8c49dd82523021f04a024c68a2bc05
Tamas Jozsa <tjozsa@codeherald.io>
2017-09-12 10:32:16 +0200

    fixing getcert.sh

--- getcert.sh
+++ getcert.sh
@@ -2,4 +2,4 @@ sudo docker run --rm  -it \
  -v "$(pwd)/out":/acme.sh \
  --net=host \
  neilpang/acme.sh \
- cme.sh  --issue -d example.com -d www.example.com --standalone
+ --issue -d example.com -d www.example.com --standalone
